864d8f498cb9a9babdf21bca7adb41c7facc84cd
jerryshao <1062963@qq.com>
2016-04-04 00:23:59 +0100

    show first company detail when the page first loaded

--- deleteCompany.php
+++ deleteCompany.php
@@ -9,6 +9,14 @@
  */
 include 'header.php';
 require_once 'functions.php';
+
+$conn = getConnection();
+$sql = "SELECT * FROM Company;";
+$counter = 0;
+//$companyArray = array();
+if(!($result = mysqli_query($conn,$sql))) {
+    die("sql script:" . mysqli_error($conn));
+}
 ?>
 	<section class="margin-top-100px">
 		<div class="form">
@@ -28,27 +36,26 @@ require_once 'functions.php';
 				<label>Company Name</label><br>
 				<select name="company" id="companies" onchange="showInfo()">
 					<?php
-					$conn = getConnection();
-					$sql = "SELECT * FROM Company;";
-					if(!($result = mysqli_query($conn,$sql))) {
-						die("sql script:" . mysqli_error($conn));
-					}
+
 					while($row = mysqli_fetch_array($result)) {
 						echo "<option value=\"" . $row['CompanyName'] . "\">" . $row['CompanyName'] . "</option>";
+                        $companyArray[$counter] = $row['CompanyName'] . "," . $row['Street'] . "," . $row['Town'] . "," . $row['County'] . "," . $row['PhoneNo'] . "," . $row['CreditLimit'];
 //						echo "<span class=\"hidden\" id=\"" . $row['CompanyName'] . "\">" . $row['Street'] . " " . $row['Town'] . " " . $row['County'] . " " . $row['PhoneNo'] . " " . " " . $row['CreditLimit'] . "</span>";
 					}
+
+                    $firstCompany = explode(",", $companyArray[0]);
 					?>
 				</select>
 				<label>Street</label><br>
-				<input type="text"><br>
+				<input type="text" value="<?php echo $firstCompany[0];?>"><br>
 				<label>Town</label><br>
-				<input type="text"><br>
+				<input type="text" value="<?php echo $firstCompany[1];?>"><br>
 				<label>County</label><br>
-				<input type="text"><br>
+				<input type="text" value="<?php echo $firstCompany[2];?>"><br>
 				<label>Telephone Number</label><br>
-				<input type="text"><br>
+				<input type="text" value="<?php echo $firstCompany[3];?>"><br>
 				<label>Credit Limit</label><br>
-				<input type="number"><br>
+				<input type="number" value="<?php echo $firstCompany[4];?>"><br>
 				<br>
 				<div class="btn-group">
 					<button class="btnGreen">Delete</button>
