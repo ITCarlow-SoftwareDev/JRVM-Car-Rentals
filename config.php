c3638577f0bf3ce2917ad7b1662a471539b33381
jerry-shao <com.jerryshao@jerryshao.com>
2016-04-18 19:56:23 +0100

    Update config.php

--- config.php
+++ config.php
@@ -4,6 +4,8 @@
  * Student ID: C00188468
  * Date: 30-03-2016
  * Purpose: Database info.
+ * Note:
+ *   Thanks a lot for someone give me a notice in database.
  */
     define('MYSQL_HOSTNAME', 'HOSTNAME');
     define('MYSQL_USERNAME', 'USERNAME');
